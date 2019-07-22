91b90537a031efa54e09cb19fe02cb28a9f570f7
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-22 14:31:05 -0700

    fix prototype

--- src/CommandLines/createReactApp.ts
+++ src/CommandLines/createReactApp.ts
@@ -2,7 +2,7 @@ import exec from './exec';
 import CommandLineException from '../exceptions/CommandLineException';
 import { ExecException } from "child_process";
 
-const createReactApp = async (appName : string, path) => {
+const createReactApp = async (appName : string, path : string) => {
     try {
         const directoryPath = `${path}/${appName}`;
         await exec(`npx create-react-app ${directoryPath}`);
