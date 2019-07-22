777d7a25f1601f78465c64df3280096d9835e0ee
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-22 14:38:52 -0700

    remove useless console.log

--- src/CommandLines/createReactApp.ts
+++ src/CommandLines/createReactApp.ts
@@ -6,7 +6,6 @@ const createReactApp = async (appName : string, path : string) => {
     try {
         const directoryPath = `${path}/${appName}`;
         await exec(`npx create-react-app ${directoryPath}`);
-        console.log(`Directory "${path}/${appName}" created`);
     } catch (e) {
         throw new CommandLineException((e as ExecException) ? `An error occurred: ${e.stdout}` : `An unknown error occurred`);
     }
