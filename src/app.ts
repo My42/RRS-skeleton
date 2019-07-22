adfcafee85a475acae48dc49129329191e804b8b
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-21 18:37:42 -0700

    add path as a mandatory argv

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ const isAnOption = arg => arg.startsWith('--');
 
 const options = process.argv.filter(isAnOption);
 
-const [appName] = process.argv.filter(arg => !isAnOption(arg)).slice(2);
+const [appName, path] = process.argv.filter(arg => !isAnOption(arg)).slice(2);
 
 if (options.some(option => option === Options.HELP)) {
-    console.log(`Usage: node app.js APP_NAME [OPTIONS]
+    console.log(`Usage: node app.js APP_NAME PATH [OPTIONS]
 Create a skeleton of a react project ready for hacking with redux & saga configured
         --help      display this help and exit
     `);
 } else {
-    console.log({ appName, options });
+    console.log({ appName, path, options });
 }
 
