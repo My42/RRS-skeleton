7cdc840c719248a3532482a9fb7d5a2d9632f315
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-22 14:39:29 -0700

    add Directories enum

---
+++ src/enums/Directories.ts
@@ -0,0 +1,12 @@
+const enum Directories {
+    FEATURES = 'features',
+    UX = 'ux',
+    ATOMS = 'atoms',
+    MOLECULES = 'molecules',
+    ORGANISMS = 'organisms',
+    PAGES = 'pages',
+    REDUX = 'redux',
+    SAGA = 'saga',
+}
+
+export default Directories;
