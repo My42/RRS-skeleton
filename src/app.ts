884fc347fcc59a218eb6e3635097fc0138d357c2
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-21 18:34:17 -0700

    update naming

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ const isAnOption = arg => arg.startsWith('--');
 
 const options = process.argv.filter(isAnOption);
 
-const [projectName] = process.argv.filter(arg => !isAnOption(arg)).slice(2);
+const [appName] = process.argv.filter(arg => !isAnOption(arg)).slice(2);
 
 if (options.some(option => option === Options.HELP)) {
-    console.log(`Usage: node app.js PROJECT_NAME [OPTIONS]
+    console.log(`Usage: node app.js APP_NAME [OPTIONS]
 Create a skeleton of a react project ready for hacking with redux & saga configured
         --help      display this help and exit
     `);
 } else {
-    console.log({ projectName, options });
+    console.log({ appName, options });
 }
 
