49c5cfcf8115483ab55c63f91bb4a26b045c29e5
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-21 18:28:39 -0700

    add argv handler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+import { Options } from './enums';
+
+const isAnOption = arg => arg.startsWith('--');
+
+const options = process.argv.filter(isAnOption);
+
+const [projectName] = process.argv.filter(arg => !isAnOption(arg)).slice(2);
+
+if (options.some(option => option === Options.HELP)) {
+    console.log(`Usage: node app.js PROJECT_NAME [OPTIONS]
+Create a skeleton of a react project ready for hacking with redux & saga configured
+        --help      display this help and exit
+    `);
+} else {
+    console.log({ projectName, options });
+}
+
