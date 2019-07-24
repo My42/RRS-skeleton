37fa9fa41ed750ff20a3f388a031db536be77432
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-24 15:23:54 -0700

    fix Yarn : add cwd

--- src/CommandLines/yarn.ts
+++ src/CommandLines/yarn.ts
@@ -2,10 +2,11 @@ import exec from './exec';
 import YarnCommands from '../enums/YarnCommands';
 import CommandLineException from '../exceptions/CommandLineException';
 
-export default async function (command : YarnCommands, args: string[]) : Promise<void> {
+export default async function (command : YarnCommands, args: string[], cwd: string) : Promise<void> {
     try {
-        await exec(`yarn ${command} ${args.join(' ')}`);
+        await exec(`yarn ${command} ${args.join(' ')} --cwd ${cwd}`);
     } catch (e) {
+        console.log(e);
         throw new CommandLineException(e.message);
     }
 }
