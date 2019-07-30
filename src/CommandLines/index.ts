4367b3c432cf1b61f82af44a1c09dffa33c5d93f
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-29 17:04:05 -0700

    update command lines index

--- src/CommandLines/index.ts
+++ src/CommandLines/index.ts
@@ -3,5 +3,6 @@ import exec from './exec';
 export { default as createReactApp } from './createReactApp'
 export { default as mkdir } from './mkdir';
 export { default as yarn } from './yarn';
+export { default as cp } from './cp';
 
 export default exec;
