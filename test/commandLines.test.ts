80de14dc2177d86849ad39d4fe0827c6c67e08ae
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-07-29 17:40:46 -0700

    add cp command's tests

--- test/commandLines.test.ts
+++ test/commandLines.test.ts
@@ -1,8 +1,11 @@
 import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
-import exec from '../src/CommandLines';
+import exec, { cp } from '../src/CommandLines';
+import { readFile as _readFile, unlink } from 'fs';
+import { promisify } from 'util';
 
 const { expect } = chai;
+const readFile = promisify(_readFile);
 
 before(() => {
     chai.should();
@@ -17,5 +20,25 @@ describe('Exec command', () => {
 
     it('should throw an error when the command is bad', async () => {
         return expect(exec('bad command')).to.be.rejectedWith(Error);
+    });
+});
+
+describe('cp command', () => {
+   it('should copy a file', async () => {
+       const srcPath = 'test/commandLines.test.ts';
+       const tmpPath = 'test/tmp';
+
+        await cp(srcPath, tmpPath);
+        const srcContent = await readFile(srcPath, 'utf8');
+        const tmpContent = await readFile(tmpPath, 'utf8');
+        expect(srcContent).to.be.equal(tmpContent, 'expected the content of src file be equal to the content of dest file');
+        unlink(tmpPath, (err) => { if (err) console.log("Couldn't remove test/tmp file") });
+   });
+
+    it ('should throw an error when src path is bad', async () => {
+        const srcPath = 'INVALID_PATH';
+        const tmpPath = 'tmp';
+
+        return expect(cp(srcPath, tmpPath)).to.be.rejectedWith(Error);
     });
 });
