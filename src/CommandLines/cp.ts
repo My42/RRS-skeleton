834752b0ac64e716c6494b49d0f188fd4a5b0ed5
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-08-25 12:39:58 -0700

    fix cp: works also on windows

--- src/CommandLines/cp.ts
+++ src/CommandLines/cp.ts
@@ -1,9 +1,9 @@
-import exec from './exec';
+import { copy } from 'fs-extra';
 import CommandLineException from '../exceptions/CommandLineException';
 
 export default async function (src : string, dest : string) : Promise<void> {
     try {
-        await exec(`cp ${src} ${dest}`);
+        await copy(src, dest);
     } catch (e) {
         throw new CommandLineException(e.message);
     }
